f6f6679d276d61177526a5074ae6c1bbeba2a590
billythedummy <dhy1996@live.com.sg>
2023-01-27 13:56:36 +0800

    missing max()

--- common/src/fees.rs
+++ common/src/fees.rs
@@ -8,6 +8,7 @@ pub struct AfterFees {
 pub fn apply_global_fee(amount: u64) -> AfterFees {
     // cast-safety: GLOBAL_FEE_BPS is < 10_000, no overflow should happen
     let fee = ((amount as u128) * (GLOBAL_FEE_BPS as u128) / 10_000u128) as u64;
+    let fee = fee.max(1);
     let remainder = amount - fee;
     AfterFees { fee, remainder }
 }
