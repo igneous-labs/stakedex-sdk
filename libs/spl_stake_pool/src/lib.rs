ace948837fafaec007110c1edea7d624c5d2b094
billythedummy <dhy1996@live.com.sg>
2023-07-02 22:55:57 +0800

    missing cogent

--- libs/spl_stake_pool/src/lib.rs
+++ libs/spl_stake_pool/src/lib.rs
@@ -1,7 +1,8 @@
 use lazy_static::lazy_static;
 use solana_program::pubkey::Pubkey;
 use stakedex_sdk_common::{
-    daopool_stake_pool, jito_stake_pool, jpool_stake_pool, laine_stake_pool, solblaze_stake_pool,
+    cogent_stake_pool, daopool_stake_pool, jito_stake_pool, jpool_stake_pool, laine_stake_pool,
+    solblaze_stake_pool,
 };
 use std::collections::HashMap;
 
@@ -11,6 +12,7 @@ pub use stakedex_traits::*;
 lazy_static! {
     pub static ref SPL_STAKE_POOL_STATE_TO_LABEL: HashMap<Pubkey, &'static str> = {
         let mut m = HashMap::new();
+        m.insert(cogent_stake_pool::ID, "Cogent");
         m.insert(daopool_stake_pool::ID, "DaoPool");
         m.insert(jito_stake_pool::ID, "Jito");
         m.insert(jpool_stake_pool::ID, "JPool");
