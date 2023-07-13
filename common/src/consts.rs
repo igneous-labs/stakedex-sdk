4fa9aa06e4f8bca0de7a6f2eadc61e336bcbf780
billythedummy <dhy1996@live.com.sg>
2023-07-13 14:47:24 +0800

    onchain prog updated

--- common/src/consts.rs
+++ common/src/consts.rs
@@ -1,11 +1,8 @@
 // hardcode for simplicity. Need to refactor when rent becomes variable.
 pub const STAKE_ACCOUNT_RENT_EXEMPT_LAMPORTS: u64 = 2_282_880;
 
-/// TODO: change to 4 when payer is removed
-pub const DEPOSIT_STAKE_DST_TOKEN_ACCOUNT_INDEX: usize = 5;
+pub const DEPOSIT_STAKE_DST_TOKEN_ACCOUNT_INDEX: usize = 4;
 
-/// TODO: change to 5 when payer is removed
-pub const SWAP_VIA_STAKE_SRC_TOKEN_MINT_ACCOUNT_INDEX: usize = 6;
+pub const SWAP_VIA_STAKE_SRC_TOKEN_MINT_ACCOUNT_INDEX: usize = 5;
 
-/// TODO: change to 6 when payer is removed
-pub const SWAP_VIA_STAKE_DST_TOKEN_MINT_ACCOUNT_INDEX: usize = 7;
+pub const SWAP_VIA_STAKE_DST_TOKEN_MINT_ACCOUNT_INDEX: usize = 6;
